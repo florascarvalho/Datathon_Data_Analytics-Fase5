16564d222192cf0fbbd68be085c3b2bc43ba3f07
Flora Serafim de Carvaho <134112749+florascarvalho@users.noreply.github.com>
2025-05-18 18:47:02 -0300

    Add files via upload

---
+++ streamlit/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.title("Datathon - Fase 05")
+
+
+st.header("Objetivo", divider="gray")
+st.subheader("""
+### Aluna: Flora Serafim de Carvalho | RM: RM354000
+""")
+
+st.write("""
+Desenvolver uma solução de IA para a melhoria do recrutamento da empresa Decision, otimizando o tempo do processo seletivo e a assertividade na contratação. 
+Considerando o histórico e a base de dados compartilhada, foi desenvolvido um modelo que auxilia no processo, identificando se o candidato possui chances de contratação e por isso deve ser chamado para entrevista e cadastrado em um banco de talentos, ou se o currículo deverá ser desconsiderado.
+
+""")
+
+st.sidebar.success("🎓Aluna: Flora Serafim de Carvalho | RM: RM354000")
